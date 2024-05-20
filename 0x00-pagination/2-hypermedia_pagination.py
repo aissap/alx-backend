e8cbe67e612fb3d034e4048781f5fb669426b9f2
aissap <jesus.jesus.core@gmail.com>
2024-05-20 23:34:01 +0000

    hypermedia

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -44,10 +44,8 @@ class Server:
             return []
         return dataset[begin:finish]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """Return hypermedia pagination details."""
-        assert isinstance(page, int) and page > 0,
-        assert isinstance(page_size, int) and page_size > 0,
 
         dataset = self.dataset()
         total_pages = math.ceil(len(dataset) / page_size)
