1d04d11917e9797419741e1c868987ec28d586a0
aissap <jesus.jesus.core@gmail.com>
2024-05-28 22:44:35 +0000

    Basic Flask app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Basic Babel setup.
+Basic Flask app.
 """
 from flask import Flask, render_template
 from flask_babel import Babel
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match with our supported languages."""
+    """Get match with the supported languages."""
     return request.accept_languages.best_match(
             app.config['LANGUAGES']
             )
--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 """
-Basic Babel setup.
+Basic Flask app.
 """
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
 
 class Config:
-    """Configuration  for Babel."""
+    """Configuration Babel."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -20,7 +20,7 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    """Determine the best match with our supported languages."""
+    """Get match with the  supported languages."""
     return request.accept_languages.best_match(
             app.config['LANGUAGES']
             )
