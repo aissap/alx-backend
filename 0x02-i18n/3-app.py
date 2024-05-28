b9593915c2cd3a066417e544c46295ca3e80a5f2
aissap <jesus.jesus.core@gmail.com>
2024-05-28 23:50:40 +0000

    translations/

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,7 +19,7 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Get match with the  supported languages."""
     return request.accept_languages.best_match(
             app.config['LANGUAGES']
@@ -27,7 +27,7 @@ def get_locale():
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """Route for the index page."""
     return render_template('3-index.html')
 
