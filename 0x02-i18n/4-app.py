b964494c91f89210f6177df019aaf6a97fb62536
aissap <jesus.jesus.core@gmail.com>
2024-05-29 00:44:32 +0000

    4-app.py

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -20,7 +20,7 @@ babel = Babel(app)
 def get_locale() -> str:
     """Get match with the supported languages."""
     locale_param = request.args.get('locale')
-    if locale_param and locale_param in app.config['LANGUAGES']:
+    if locale_param in app.config['LANGUAGES']:
         return locale_param
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
