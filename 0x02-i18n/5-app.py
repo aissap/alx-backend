1f56a7e852bc56313b35514d4cdb09f16a48fcb8
aissap <jesus.jesus.core@gmail.com>
2024-05-29 01:29:02 +0000

    Mock logging in

---
+++ 0x02-i18n/5-app.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Configuration Babel."""
+    DEBUG = True
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user(user_id: int) -> Union[dict, None]:
+    '''Get user information based on user ID'''
+    return users.get(user_id)
+
+
+@app.before_request
+def before_request():
+    '''Set user information as global variable'''
+    user_id = request.args.get('login_as')
+    g.user = get_user(int(user_id)) if user_id else None
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Get match with the supported languages."""
+    locale_param = request.args.get('locale')
+    if locale_param in app.config['LANGUAGES']:
+        return locale_param
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index() -> str:
+    """Route for the index page."""
+    return render_template("4-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
