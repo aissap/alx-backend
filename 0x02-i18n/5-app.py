05dc04bac4613336b23093f3c3f851aa64705f7f
aissap <jesus.jesus.core@gmail.com>
2024-05-29 01:31:30 +0000

    Mock logging in

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+from flask import Flask, render_template, g, request
 from flask_babel import Babel
+from typing import Union
 
 
 class Config:
