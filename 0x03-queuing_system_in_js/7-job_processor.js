0cf46100db1753f4799dc33a696885b3477167c8
aissap <jesus.jesus.core@gmail.com>
2024-06-27 00:17:12 +0000

    7-job_creator.js

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,25 @@
+const kue = require('kue');
+
+const blacklist = ['4153518780', '4153518781'];
+
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+
+  if (blacklist.includes(phoneNumber)) {
+    return done(new Error(`Phone number ${phoneNumber} is blacklisted`));
+  }
+
+  job.progress(50, 100);
+
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+
+  done();
+}
+
+const que = kue.createQueue();
+
+que.process('push_notification_code_2', 2, function (job, done) {
+  const { phoneNumber, message } = job.data;
+
+  sendNotification(phoneNumber, message, job, done);
+});
