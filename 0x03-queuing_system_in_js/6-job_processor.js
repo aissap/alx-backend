2816f51d3f465d6d9ee89449ab1930e187e81d3d
aissap <jesus.jesus.core@gmail.com>
2024-06-27 00:22:22 +0000

    8-job

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -1,34 +1,15 @@
 const kue = require('kue');
-const redis = require('redis');
 
-const client = redis.createClient();
-const queue = kue.createQueue({
-  prefix: 'q',
-  redis: {
-    createClientFactory: () => client
-  }
-});
+const que = kue.createQueue();
 
-function sendNotification (phoneNumber, message) {
+function sendNotification(phoneNumber, message) {
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
-queue.process('push_notification_code', function (job, done) {
+que.process('push_notification_code', (job, done) => {
   const { phoneNumber, message } = job.data;
   sendNotification(phoneNumber, message);
   done();
-});
-
-queue.on('error', function (err) {
-  console.log('Kue Error:', err);
-});
-
-process.once('SIGTERM', function (sig) {
-  queue.shutdown(5000, function (err) {
-    console.log('Kue shutdown:', err || 'OK');
-    client.quit();
-    process.exit(0);
-  });
 });
 
 console.log('Job processor is running...');
